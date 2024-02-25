5f169e2800665d870b5e48ed181227db90bf6d64
koson <567256+koson@users.noreply.github.com>
2024-02-26 06:37:53 +0700

    Lab5 es2

---
+++ Lab05_Ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿Person p = new Person();
+p.name = "Rambo";
+p.id = "1987";
+p.income = 250000;
+System.Console.WriteLine($"Type of p is {p.GetType()}");
+
+class Person
+{
+    string name;
+    string id;
+    int income;
+}
