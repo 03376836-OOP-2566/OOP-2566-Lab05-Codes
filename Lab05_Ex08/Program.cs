d4a27824a5f3c6e43dbe840bdcb97583b0dd8dd7
koson <567256+koson@users.noreply.github.com>
2024-02-26 06:45:19 +0700

    Lab 5 ex 8

--- Lab05_Ex08/Program.cs
+++ Lab05_Ex08/Program.cs
@@ -1,10 +1,14 @@
-﻿int a = 2, b = 3;
-int add_result = Calculator.add(a,b);
-System.Console.WriteLine($"{a} + {b} = {add_result}");
+﻿int x = 2, y = 3;
+int add_result = Calculator.add(x, y);
+System.Console.WriteLine($"{x} + {y} = {add_result}");
 
-static class Calculator
+int i = 2, j = 3;
+int multiply_result = Calculator.multiply(i, j);
+System.Console.WriteLine($"{i} x {j} = {multiply_result}");
+
+public static class Calculator
 {
-    public  static int add(int a, int b)
+    public static int add(int a, int b)
     {
         return a + b;
     }
